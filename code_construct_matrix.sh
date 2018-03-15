3882c1ea5ad3416a790c01bea65fa0d3823707ff
fwip <pink.fwip@gmail.com>
2018-03-15 11:32:02 -0700

    Fix up existing scripts

--- code_construct_matrix.sh
+++ code_construct_matrix.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ##########################################################################################################
 #                                                                                                        #
 # Method for generating a master list / Index of DNaseI hypersensitivity sites.                          #
@@ -8,37 +9,37 @@
 ##########################################################################################################
 
 
-if [[ $# -eq 0 ]] ; then
-    echo 'Provide the name/version of the file, e.g. WM20180301'
+if [[ $# -le 2 ]] ; then
+    echo 'Provide the name/version of the file, e.g. WM20180301, and the number of chunks'
     exit 1
 fi
 
 NAME=$1;
+numchunks=$2
 
-rm -f matrix_bin_all_${NAME}.txt.gz;
-for i in $(seq -f "%04g" 1 5000); do
-  echo ${i}
+rm -f "matrix_bin_all_${NAME}.txt.gz"
+for i in $(seq -f "%04g" 1 "$numchunks"); do
+  echo "$i"
   if [[ -f "DHSs_all/chunk${i}.bed" ]] ; then
     #paste <(cut -f 1-3 DHSs_all/chunk${i}.bed) matrix_bin_all/chunk${i}.bed | gzip -c - >> matrix_bin_all_${NAME}.txt.gz
-    paste <(cut -f 4 DHSs_all/chunk${i}.bed) matrix_bin_all/chunk${i}.bed | gzip -c - >> matrix_bin_all_${NAME}.txt.gz
+    paste <(cut -f 4 "DHSs_all/chunk${i}.bed") "matrix_bin_all/chunk${i}.bed" | gzip -c - >> "matrix_bin_all_${NAME}.txt.gz"
   fi
 done
 
-rm -f matrix_bin_nonovl_core_${NAME}.txt.gz;
-for i in $(seq -f "%04g" 1 5000); do
-  echo ${i}
+rm -f "matrix_bin_nonovl_core_${NAME}.txt.gz"
+for i in $(seq -f "%04g" 1 "$numchunks"); do
+  echo "$i"
   if [[ -f "DHSs_nonovl_core/chunk${i}.bed" ]] ; then
     #paste <(cut -f 1-3 DHSs_nonovl_core/chunk${i}.bed) matrix_bin_nonovl_core/chunk${i}.bed | gzip -c - >> matrix_bin_nonovl_core_${NAME}.txt.gz
-    paste <(cut -f 4 DHSs_nonovl_core/chunk${i}.bed) matrix_bin_nonovl_core/chunk${i}.bed | gzip -c - >> matrix_bin_nonovl_core_${NAME}.txt.gz
+    paste <(cut -f 4 "DHSs_nonovl_core/chunk${i}.bed") "matrix_bin_nonovl_core/chunk${i}.bed" | gzip -c - >> "matrix_bin_nonovl_core_${NAME}.txt.gz"
   fi
 done
 
-rm -f matrix_bin_nonovl_any_${NAME}.txt.gz;
-for i in $(seq -f "%04g" 1 5000); do
-  echo ${i}
+rm -f "matrix_bin_nonovl_any_${NAME}.txt.gz"
+for i in $(seq -f "%04g" 1 "$numchunks"); do
+  echo "$i"
   if [[ -f "DHSs_nonovl_any/chunk${i}.bed" ]] ; then
     #paste <(cut -f 1-3 DHSs_nonovl_any/chunk${i}.bed) matrix_bin_nonovl_any/chunk${i}.bed | gzip -c - >> matrix_bin_nonovl_any_${NAME}.txt.gz
-    paste <(cut -f 4 DHSs_nonovl_any/chunk${i}.bed) matrix_bin_nonovl_any/chunk${i}.bed | gzip -c - >> matrix_bin_nonovl_any_${NAME}.txt.gz
+    paste <(cut -f 4 "DHSs_nonovl_any/chunk${i}.bed") "matrix_bin_nonovl_any/chunk${i}.bed" | gzip -c - >> "matrix_bin_nonovl_any_${NAME}.txt.gz"
   fi
 done
-
--- code_gen_masterlist.sh
+++ code_gen_masterlist.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ##########################################################################################################
 #                                                                                                        #
 # Method for generating a master list / Index of DNaseI hypersensitivity sites.                          #
@@ -16,7 +17,7 @@ NAME=$1;
 
 TYPES="all nonovl_any nonovl_core";
 for TYPE in ${TYPES}; do
-  echo $TYPE;
+  echo "$TYPE"
 
   FILE_CHUNKIDS="masterlist_DHSs_${NAME}_${TYPE}_chunkIDs.txt";
   FILE_INDEXIDS="masterlist_DHSs_${NAME}_${TYPE}_indexIDs.txt";
@@ -26,22 +27,22 @@ for TYPE in ${TYPES}; do
   ### Create final master list
   if ! [[ -f "$FILE_CHUNKIDS" ]] ; then
     echo "Concatenating DHS chunks"
-    cat DHSs_${TYPE}/* | sort-bed - > ${FILE_CHUNKIDS}
+    cat "DHSs_${TYPE}"/* | sort-bed - > "${FILE_CHUNKIDS}"
   fi
-  
+
   ### Generate label mapping
-  if ! [[ -f "masterlist_DHSs_${NAME}_all_chunkIDs2indexIDs.txt" && ${TYPE}=="all" ]] ; then
+  if ! [[ -f "masterlist_DHSs_${NAME}_all_chunkIDs2indexIDs.txt" && ${TYPE} == "all" ]] ; then
     echo "Generating unique DHS identifiers"
-    ./run_name_master_list.sh ${FILE_CHUNKIDS};
+    ./run_name_master_list.sh "${FILE_CHUNKIDS}"
   fi
-  
+
   ### Apply mapping
   if ! [[ -f "$FILE_INDEXIDS" ]] ; then
     echo "Mapping chunk identifiers to final DHS identifiers"
     awk 'BEGIN{ FS = OFS = "\t" }
-      FNR == NR { split($0, f, /\t/); map[f[2]] = f[1]; next } 
-      { if ($4 in map) { $4 = map[$4] } } 
-      { print }' masterlist_DHSs_${NAME}_all_chunkIDs2indexIDs.txt ${FILE_CHUNKIDS} > ${FILE_INDEXIDS}
+      FNR == NR { split($0, f, /\t/); map[f[2]] = f[1]; next }
+      { if ($4 in map) { $4 = map[$4] } }
+      { print }' "masterlist_DHSs_${NAME}_all_chunkIDs2indexIDs.txt" "${FILE_CHUNKIDS}" > "${FILE_INDEXIDS}"
   fi
 
   ### Create browser loadable BED12 files
@@ -49,9 +50,9 @@ for TYPE in ${TYPES}; do
     echo "Constructing BED12 file"
     #echo "browser position chr6:26020208-26022677" > ${FILE_BED}
     #echo "track name='Master list DHSs ${NAME} ${TYPE}' description='Master list DHSs ${NAME} ${TYPE}' visibility=2 useScore=1" >> ${FILE_BED}
-    cat ${FILE_INDEXIDS} | awk '
+    awk "${FILE_INDEXIDS}" '
     function round(x) { if(x=="NA") { return 0 } else { return int(x + 0.5) } }
-    BEGIN { OFS="\t"; } 
+    BEGIN { OFS="\t"; }
     {
       if ($10 == "NA" || $11 == "NA") {
         thickStart=$2
@@ -64,51 +65,49 @@ for TYPE in ${TYPES}; do
         $9=($9 >= $3 ? $3-1 : $9)
         $10=($10 <= $2 ? $2+1 : $10)
         $11=($11 >= $3 ? $3-1 : $11)
-    
+
         thickStart= $9-1
         thickEnd  = $9+1
-    
+
         blockCount=1
         blockSizes=1
         blockStarts=0
-    
+
         blockSize2=$9-$10
         blockStart2=$10-$2
-        if (blockSize2 != 0 && blockStart2 < $3-$2-1) { 
-          blockCount+=1; 
-          blockSizes=blockSizes","blockSize2; 
-          blockStarts=blockStarts","blockStart2 
+        if (blockSize2 != 0 && blockStart2 < $3-$2-1) {
+          blockCount+=1;
+          blockSizes=blockSizes","blockSize2;
+          blockStarts=blockStarts","blockStart2
         }
-    
+
         blockSize3=$11-$9
         blockStart3=$9-$2
-        if (blockSize3 != 0 && blockStart3 < $3-$2-1) { 
-          blockCount+=1; 
-          blockSizes=blockSizes","blockSize3; 
-          blockStarts=blockStarts","blockStart3 
+        if (blockSize3 != 0 && blockStart3 < $3-$2-1) {
+          blockCount+=1;
+          blockSizes=blockSizes","blockSize3;
+          blockStarts=blockStarts","blockStart3
         }
-    
+
         blockSize4=1
         blockStart4=$3-$2-1
         blockCount+=1;
-        blockSizes=blockSizes","blockSize4; 
+        blockSizes=blockSizes","blockSize4;
         blockStarts=blockStarts","blockStart4
       }
-  
+
       score=round(log($5+1)/log(10)*500)
       score=(score > 1000 ? 1000 : score)
       print $1, $2, $3, $4, score, ".", thickStart, thickEnd, "0,0,0", blockCount, blockSizes, blockStarts
-    }' > ${FILE_BED}
+    }' > "${FILE_BED}"
   fi
-  
+
   if ! [[ -f "$FILE_BIGBED" ]] ; then
     echo "Converting BED file to BIGBED file"
-    bedToBigBed -type=bed12 ${FILE_BED} <(tail -n +2 hg38.genome) ${FILE_BIGBED}
+    bedToBigBed -type=bed12 "${FILE_BED}" <(tail -n +2 hg38.genome) "${FILE_BIGBED}"
   fi
 
   echo "Load this track in the UCSC browser using the following:"
   echo "track type=bigBed name=master_list_${NAME}_${TYPE} useScore=1 visibility=2 itemRgb='On' bigDataUrl=https://encode:collabor8@resources.altius.org/~meuleman/ML_tracks/${FILE_BIGBED}"
 
-done;
-
-
+done
