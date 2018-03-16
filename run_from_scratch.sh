9b59832156fe3cde53a2f177e25e7d5b7d10597e
fwip <pink.fwip@gmail.com>
2018-03-16 13:16:55 -0700

    Fix awk & dir errors in run_from_scratch

--- run_from_scratch.sh
+++ run_from_scratch.sh
@@ -74,6 +74,7 @@ do
 done < "$FILE_OF_BAM_FILES"
 
 
+here=$(dirname "$(readlink -f "$0")")
 OUTFILE=allPeaks.starch
 {
     echo -e "Collating peaks..."
@@ -90,24 +91,26 @@ OUTFILE=allPeaks.starch
        | awk -v minChunkSep=10000 -v minChunkSep2=4000 -v minPerChunk=500 -v maxPerChunk=100000 \
           'BEGIN{n=1; nWritten=0; outf="chunk0001.bed"}{
              if(NR > 1){
-                if(\$1 != prevChr || \$2-prev3 > minChunkSep){
+                if($1 != prevChr || $2-prev3 > minChunkSep){
                    if(nWritten >= minPerChunk){
                       outf = sprintf("chunk%04d.bed", ++n);
                       nWritten = 0;
                     }
                   }
                 else{
-                   if(\$2-prev3 > minChunkSep2 && nWritten > maxPerChunk){
+                   if($2-prev3 > minChunkSep2 && nWritten > maxPerChunk){
                       outf = sprintf("chunk%04d.bed",++n);
                       nWritten=0;
                     }
                   }
                 }
-             print \$0 > outf;
-             nWritten++; prevChr = \$1; prev3 = \$3;
+             print $0 > outf;
+             nWritten++; prevChr = $1; prev3 = $3;
            }'
     echo -e "Done!"
 }
 
+cd "$here"
+
 # Run the masterlist stuff
-./run_sequential.sh "$OUTDIR" "$OUTDIR"
+"$here/run_sequential.sh" "$OUTDIR/subdir" "$CHROM_FILTER"
